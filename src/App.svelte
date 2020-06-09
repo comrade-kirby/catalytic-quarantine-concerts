3e07ffb3f5b44898a48d19861862cf1d2bc62774
Max Oppenheimer <maxoppenheimer1993@gmail.com>
2020-06-09 14:55:53 -0500

    initial commit;

---
+++ src/App.svelte
@@ -0,0 +1,23 @@
+<script>
+	import Schedule from './components/Schedule/Schedule.svelte'
+	import Stage from './components/Stage/Stage.svelte'
+</script>
+
+<main>
+	<Schedule />
+	<Stage />
+</main>
+
+<style>
+	main {
+	
+	}
+
+	
+
+	@media (min-width: 640px) {
+		main {
+
+		}
+	}
+</style>
---
+++ src/components/Schedule/Schedule.svelte
@@ -0,0 +1,1 @@
+<div>this is a schedule component</div>
