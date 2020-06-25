aab877ac32b488f0c8ae712b16d384ac54987735
Max Oppenheimer <maxoppenheimer1993@gmail.com>
2020-06-25 13:00:15 -0500

    import anime js

--- src/components/Home/Home.svelte
+++ src/components/Home/Home.svelte
@@ -1,13 +1,34 @@
+<script>
+  import { onMount } from 'svelte'
+  import anime from 'animejs/lib/anime.es.js'
 
+  onMount(() => {
+
+    anime({
+      targets: '.test-anime',
+      translateX: 250,
+      rotate: '1turn',
+      backgroundColor: '#FFF',
+      duration: 800
+    });
+  })
+</script>
 
 <div class='home'>
+  <div class='test-anime'>
+  </div>
   <h3 class='catalytic-sound'>catalytic sound</h3>
   <h1 class='festival'>Festival 2020</h1>
   <h3 class='dates-month'>July 10-12</h3>
 </div>
 
 <style>
-
+  .test-anime {
+    position: relative;
+    height: 300px;
+    width: 400px;
+    background-color: red;
+  }
   .catalytic-sound {
     margin-bottom: 20px;
   }
