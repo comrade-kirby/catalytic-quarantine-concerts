be8cfff64618cebc6d819c9f0c14eed5618132b5
Max Oppenheimer <maxoppenheimer1993@gmail.com>
2020-06-25 16:06:38 -0500

    break 'festival' into separate component

---
+++ src/components/Home/Festival2020/Festival/Festival.svelte
@@ -0,0 +1,29 @@
+<script>
+  import { onMount } from 'svelte'
+  import anime from 'animejs/lib/anime.es.js'
+
+  onMount(() => {
+    const elements = document.querySelectorAll('.letter');
+
+    anime({
+      targets: elements,
+      translateY: 20,
+      opacity: [0, 1],
+      delay: anime.stagger(20, {start: 1600, from: 'first'}),
+      easing: 'easeInOutExpo'
+    });
+  })
+</script>
+
+{#each "festival".split("") as letter }
+  <h1 class='letter'>{letter}</h1>
+{/each}
+
+<style>
+  .letter {
+    position: relative;
+    top: -20px;
+    font-family: var(--sans-serif-2);
+    margin-right: 8px;
+  }
+</style>
