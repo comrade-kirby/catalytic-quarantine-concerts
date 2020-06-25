fb1c2f529454f5689aa7a8136e35f77632ccdff0
Max Oppenheimer <maxoppenheimer1993@gmail.com>
2020-06-25 16:14:35 -0500

    animate '2020' in home

--- src/components/Home/Festival2020/Year/Year.svelte
+++ src/components/Home/Festival2020/Year/Year.svelte
@@ -2,7 +2,7 @@
   import { onMount } from 'svelte'
   import anime from 'animejs/lib/anime.es.js'
 
-  let height
+  let width
 
 
   onMount(() => {
@@ -10,23 +10,20 @@
     const containers = document.querySelectorAll('.twenty-container')
     anime({
       targets: elements,
-      rotateX: [90, 0],
-      opacity: [0.7, 1],
-      delay: anime.stagger(250, {start: 2500, from: 'first'}),
-      easing: 'linear',
-      duration: 300
+      rotateY: [90, 0],
+      delay: anime.stagger(300, {start: 2100, from: 'first'}),
+      easing: 'easeInOutQuint',
     })
     anime({
       targets: containers,
-      translateY: [-height / 4, 0],
-      delay: anime.stagger(250, {start: 2500, from: 'first'}),
-      easing: 'linear',
-      duration: 300
+      translateX: [width / 4, 0],
+      delay: anime.stagger(300, {start: 2100, from: 'first'}),
+      easing: 'easeInOutQuint',
     })
   })
 </script>
 
-<div class='year' bind:clientHeight={height}>
+<div class='year' bind:clientWidth={width}>
   <div class='twenty-container'>
     <h3 class='twenty'>20</h3>
   </div>
@@ -40,6 +37,9 @@
     display: flex;
     flex-direction: row;
     margin-top: 20px;
-    /* transform: rotateY(-45deg); */
+ }
+
+ .twenty {
+    font-family: var(--mono-1);
  }
 </style>
