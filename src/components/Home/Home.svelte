ed319b27798685c200196293d44d6e49c3f82d73
Max Oppenheimer <maxoppenheimer1993@gmail.com>
2020-06-25 12:15:19 -0500

    layout home component

--- src/components/Home/Home.svelte
+++ src/components/Home/Home.svelte
@@ -1,1 +1,30 @@
-<div>this is the home component</div>
+
+
+<div class='home'>
+  <h3 class='catalytic-sound'>catalytic sound</h3>
+  <h1 class='festival'>Festival 2020</h1>
+  <h3 class='dates-month'>July 10-12</h3>
+</div>
+
+<style>
+
+  .catalytic-sound {
+    margin-bottom: 20px;
+  }
+  .festival {
+    font-family: var(--sans-serif-2);
+  }
+
+  .dates-month {
+    font-family: var(--mono-1);
+  }
+
+  .home {
+    width: 100%;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    background: linear-gradient(90deg, rgba(0, 0, 0, 20%), rgba(0, 0, 0, 5%));
+    padding: 20px;
+  }
+</style>
