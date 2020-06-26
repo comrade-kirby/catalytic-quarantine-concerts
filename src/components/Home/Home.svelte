35bfeb697b298041db2ce7bbe87c08290ddd4984
Max Oppenheimer <maxoppenheimer1993@gmail.com>
2020-06-26 12:59:08 -0500

    pull left side of home component into new component

--- src/components/Home/Home.svelte
+++ src/components/Home/Home.svelte
@@ -2,23 +2,13 @@
   import { onMount } from 'svelte'
   import anime from 'animejs/lib/anime.es.js'
 
-  import CatalyticSound from './CatalyticSound/CatalyticSound.svelte'
-  import Festival from './Festival/Festival.svelte'
-  import Year from './Year/Year.svelte'
-  import Date from './Date/Date.svelte'
+  import HomeLeft from './HomeLeft/HomeLeft.svelte'
+ 
 
-  let height, rightWidth
-  const screenColumns = [...Array(30).keys()]
+  let rightWidth
 
   onMount(() => {
     const elements = document.querySelectorAll('.screen-column');
-
-    anime({
-      targets: elements,
-      translateY: height,
-      delay: anime.stagger(10, {start: 1000, from: 'last'}),
-      easing: 'easeInOutExpo'
-    });
 
     anime({
       targets: '.right-screen',
@@ -30,20 +20,8 @@
   })
 </script>
 
-<div class='home' bind:clientHeight={height}>
-  <div class='home-left'>
-    <CatalyticSound delay={1300} />
-    <div class='festival2020-container'>
-      <Festival delay={1600} />
-      <Year delay={2500} />
-    </div>
-    <Date delay={2500} />
-    <div class='left-screen'>
-      {#each screenColumns as column}
-        <div class='screen-column'></div>
-      {/each}
-    </div>
-  </div>
+<div class='home' >
+  <HomeLeft />
   <div class='home-right' bind:clientWidth={rightWidth}>
     <button class='right-button'>
       <h2 class='right-button-text'>LINEUP</h2>
@@ -60,37 +38,6 @@
     flex-direction: row;
     overflow: hidden;
     background: linear-gradient(90deg, rgba(0, 0, 0, 20%), rgba(0, 0, 0, 0%));
-  }
-
-  .home-left {
-    position: relative;
-    display: flex;
-    flex-direction: column;
-    justify-content: center;
-    align-items: flex-start;
-  }
-
-  .festival2020-container {
-    display: flex;
-    flex-direction: row;
-    flex-wrap: wrap;
-    margin: 0 20px;
-    align-items: flex-end;
-  }
-
- .left-screen {
-    display: flex;
-    flex-direction: row;
-    position: absolute;
-    top: 0;
-    width: 100%;
-    height: 100%;
-  }
-
-  .screen-column {
-    display: flex;
-    flex: 1;
-    background: aliceblue;
   }
 
   .home-right {
---
+++ src/components/Home/HomeLeft/HomeLeft.svelte
@@ -0,0 +1,72 @@
+<script>
+  import { onMount } from 'svelte'
+  import anime from 'animejs/lib/anime.es.js'
+
+  import CatalyticSound from './CatalyticSound/CatalyticSound.svelte'
+  import Festival from './Festival/Festival.svelte'
+  import Year from './Year/Year.svelte'
+  import Date from './Date/Date.svelte'
+
+  let height
+  
+  const screenColumns = [...Array(30).keys()]
+
+  onMount(() => {
+    const elements = document.querySelectorAll('.screen-column');
+
+    anime({
+      targets: elements,
+      translateY: height,
+      delay: anime.stagger(10, {start: 1000, from: 'last'}),
+      easing: 'easeInOutExpo'
+    })
+  })
+</script>
+
+<div class='home-left' bind:clientHeight={height}>
+    <CatalyticSound delay={1300} />
+    <div class='festival2020-container'>
+      <Festival delay={1600} />
+      <Year delay={2500} />
+    </div>
+    <Date delay={2500} />
+    <div class='left-screen'>
+      {#each screenColumns as column}
+        <div class='screen-column'></div>
+      {/each}
+    </div>
+  </div>
+
+
+<style>
+  .home-left {
+    position: relative;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: flex-start;
+  }
+
+  .festival2020-container {
+    display: flex;
+    flex-direction: row;
+    flex-wrap: wrap;
+    margin: 0 20px;
+    align-items: flex-end;
+  }
+
+ .left-screen {
+    display: flex;
+    flex-direction: row;
+    position: absolute;
+    top: 0;
+    width: 100%;
+    height: 100%;
+  }
+
+  .screen-column {
+    display: flex;
+    flex: 1;
+    background: aliceblue;
+  }
+</style>
