b7602c35db42df03c1ecf100580bb6a62d32c4da
Max Oppenheimer <maxoppenheimer1993@gmail.com>
2020-06-26 13:34:27 -0500

    move right button in home to separate component

--- src/components/Home/Home.svelte
+++ src/components/Home/Home.svelte
@@ -1,14 +1,13 @@
 <script>
   import FestivalInfo from './FestivalInfo/FestivalInfo.svelte'
   import SlideScreen from './SlideScreen/SlideScreen.svelte'
+  import RightButton from './RightButton/RightButton.svelte'
 </script>
 
 <div class='home' >
   <FestivalInfo />
   <SlideScreen />
-  <button class='right-button'>
-    <h2 class='right-button-text'>LINEUP</h2>
-  </button>
+  <RightButton />
 </div>
 
 <style>
@@ -18,38 +17,5 @@
     flex-direction: row;
     overflow: hidden;
     background: linear-gradient(90deg, rgba(0, 0, 0, 20%), rgba(0, 0, 0, 0%));
-  }
-
-  .right-button {
-    position: absolute;
-    right: 0;
-    margin: 0;
-    height: 100%;
-    min-width: 150px;
-    border: none;
-    border-left: 5px solid aliceblue;
-    background: rgba(0, 0, 0, 10%);
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    transition: background 0.3s ease-in-out;
-  }
-
-  .right-button-text {
-    writing-mode: vertical-rl;
-    text-orientation: upright;
-    color: aliceblue;
-    margin: 20px;
-    font-family: var(--mono-1);
-    transition: color 0.3s ease-in-out;
-
-  }
-
-  .right-button:hover {
-    background: aliceblue;
-  }
-
-  .right-button:hover .right-button-text {
-    color: navy;
   }
 </style>
---
+++ src/components/Home/RightButton/RightButton.svelte
@@ -0,0 +1,63 @@
+<script>
+  import { onMount } from 'svelte'
+  import anime from 'animejs/lib/anime.es.js'
+
+  let width
+
+  onMount(() => {
+    anime({
+      targets: '.right-button',
+      translateX: [width + 20, 0],
+      delay: 4500,
+      easing: 'easeOutExpo',
+    })
+  })
+</script>
+
+<div class='button-container'>
+  <button class='right-button' bind:clientWidth={width}>
+    <h2 class='right-button-text'>LINEUP</h2>
+  </button>
+</div>
+  
+<style>
+
+  .button-container {
+    position: absolute;
+    right: 0;
+    height: 100%;
+    overflow: hidden;
+    display: flex;
+    min-width: 150px;
+  }
+
+  .right-button {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    flex: 1;
+    margin: 0;
+    border: none;
+    border-left: 5px solid aliceblue;
+    background: rgba(0, 0, 0, 10%);
+    transition: background 0.3s ease-in-out;
+  }
+
+  .right-button-text {
+    writing-mode: vertical-rl;
+    text-orientation: upright;
+    color: aliceblue;
+    margin: 20px;
+    font-family: var(--mono-1);
+    transition: color 0.3s ease-in-out;
+
+  }
+
+  .right-button:hover {
+    background: aliceblue;
+  }
+
+  .right-button:hover .right-button-text {
+    color: navy;
+  }
+</style>
