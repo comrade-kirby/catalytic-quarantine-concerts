0e89a79be73cd82a285c98d1ea091ec9e3e7ac7b
Max Oppenheimer <maxoppenheimer1993@gmail.com>
2020-06-26 14:03:41 -0500

    give slide screen a slightly elastic intro

--- src/components/Home/SlideScreen/SlideScreen.svelte
+++ src/components/Home/SlideScreen/SlideScreen.svelte
@@ -8,18 +8,31 @@
     anime({
       targets: '.right-screen',
       translateX: width,
-      delay: 3600,
-      easing: 'easeInExpo',
+      delay: 3700,
+      easing: 'cubicBezier(0.50,-0.03,0.75,0.05)',
       duration: 800
     })
   })
 </script>
 
-<div class='right-screen' bind:clientWidth={width}></div>
+<div class='right-screen' bind:clientWidth={width}>
+  <div class='bounce-buffer'></div>
+</div>
 
 <style>
   .right-screen {
+    display: flex;
     flex: 1;
+    align-items: flex-end;
+    justify-content: flex-end;
+    background-color: aliceblue;
+  }
+
+  .bounce-buffer {
+    position: relative;
+    right: -50px;
+    width: 50px;
+    height: 100%;
     background-color: aliceblue;
   }
 </style>
