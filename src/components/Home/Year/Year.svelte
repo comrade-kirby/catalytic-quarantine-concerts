091a344f858773818b9731ca73b600a9140c9418
Max Oppenheimer <maxoppenheimer1993@gmail.com>
2020-06-25 22:14:41 -0500

    change rotation of 20s rotation and refactor for clarity

--- src/components/Home/Year/Year.svelte
+++ src/components/Home/Year/Year.svelte
@@ -2,30 +2,41 @@
   import { onMount } from 'svelte'
   import anime from 'animejs/lib/anime.es.js'
 
-  let width
+  let height
 
-
-  onMount(() => {
-    const elements = document.querySelectorAll('.twenty')
+  const translateContainers = () => {
     const containers = document.querySelectorAll('.twenty-container')
-    anime({
-      targets: elements,
-      rotateY: [120, 0],
-      opacity: [0, 1],
-      delay: anime.stagger(150, {start: 2500, from: 'first'}),
-      easing: 'easeInOutQuint',
-    })
+
     anime({
       targets: containers,
-      translateX: [width / 4, 0],
-      translateY: [-10, 0],
-      delay: anime.stagger(150, {start: 2500, from: 'first'}),
-      easing: 'easeInOutQuint',
+      translateY: [-height / 4, 0],
+      translateX: [-5, 0],
+      delay: anime.stagger(250, {start: 2200, from: 'first'}),
+      easing: 'easeInQuint',
     })
+  }
+
+  const rotateTwenties = () => {
+    const twenties = document.querySelectorAll('.twenty')
+
+    anime({
+      targets: twenties,
+      rotateX: [-100, 0],
+      rotateY: [10, 0],
+      rotateZ: [-10, 0],
+      opacity: [0, 1],
+      delay: anime.stagger(250, {start: 2200, from: 'first'}),
+      easing: 'easeInQuint',
+    })
+  }
+  
+  onMount(() => {
+    translateContainers()
+    rotateTwenties()
   })
 </script>
 
-<div class='year' bind:clientWidth={width}>
+<div class='year' bind:clientHeight={height}>
   <div class='twenty-container'>
     <h3 class='twenty'>20</h3>
   </div>
