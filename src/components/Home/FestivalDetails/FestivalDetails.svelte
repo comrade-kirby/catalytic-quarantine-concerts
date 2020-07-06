0b836c86c36fc02d193245dc280e576bf9f20fc8
Max Oppenheimer <maxoppenheimer1993@gmail.com>
2020-07-06 10:42:45 -0500

    add paypal button

--- src/components/Home/FestivalDetails/FestivalDetails.svelte
+++ src/components/Home/FestivalDetails/FestivalDetails.svelte
@@ -13,9 +13,12 @@
     <div class='festival-details-bottom'>
       <div class='donate'>
         <h5>$10/program suggested donation</h5>
-        <a href="" target="_blank" rel="noopener noreferrer">donate
-        <!-- <span class="material-icons">arrow_forward</span> -->
-        </a>
+        <form action="https://www.paypal.com/cgi-bin/webscr" method="post" target="_top">
+          <input type="hidden" name="cmd" value="_s-xclick" />
+          <input type="hidden" name="hosted_button_id" value="4E7FLHV355X9E" />
+          <input class='donate-img' type="image" src="https://www.paypalobjects.com/en_US/i/btn/btn_donateCC_LG.gif" border="0" name="submit" title="PayPal - The safer, easier way to pay online!" alt="Donate with PayPal button" />
+          <img  alt="paypal donate button" src="https://www.paypal.com/en_US/i/scr/pixel.gif" />
+        </form>
       </div>
       <p>Music, video, and live performances each day at 1 pm and 7 pm US Central Time. Discussion panel after each afternoon program.</p>
     </div>
@@ -81,12 +84,15 @@
     padding: 20px;
   }
 
-  a {
-    text-shadow: none;
-    color: midnightblue;
+  form {
     border: 2px solid midnightblue;
   }
 
+  .donate-img {
+    width: 100%;
+    margin: 0;
+    border: none;
+  }
   @media only screen and (max-width: 1200px) {
 		h5 {
       margin-top: 20px;
