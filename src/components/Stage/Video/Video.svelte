e9e63397d22f6058453ffb07ff6d884de0f5aed9
Max Oppenheimer <maxoppenheimer1993@gmail.com>
2020-07-09 13:10:56 -0500

    add parent to video

--- src/components/Stage/Video/Video.svelte
+++ src/components/Stage/Video/Video.svelte
@@ -7,6 +7,7 @@
       height: 480,
       chat: true,
       channel: "experimental_sound_studio",
+      parent: ["festival.catalyticsound.com", "festival.catalyticsound.com/stage"]
       // layout: 'video'
     });
   })
