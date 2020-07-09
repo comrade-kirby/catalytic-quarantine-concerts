12c18ca9e2dbdc87f33428f0d68a7096c05827b1
Max Oppenheimer <maxoppenheimer1993@gmail.com>
2020-07-09 13:52:31 -0500

    refactor schedule header into reusable component

---
+++ src/components/Header/Header.svelte
@@ -0,0 +1,60 @@
+<script>
+  export let headerText
+  export let outro
+</script>
+
+<div class='header-container' class:outro>
+  {#each headerText.split("") as letter }
+    <h3 class='button-letter'>{letter}</h3>
+  {/each}
+</div>
+
+<style>
+  .header-container {
+    display: flex;
+    height: 100%;
+    z-index: 1;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    padding: 6.4px;
+    background-color: aliceblue;
+    transition: background-color 1s ease-in-out 0.45s, border-right 1s ease-in-out 0.45s;
+  }
+
+  .button-letter {
+    margin: 10px 30px;
+    color: black;
+    text-shadow: none;
+    transition: color 1s ease-in-out 0.45s;
+  }
+
+  .header-container.outro {
+    background-color: rgba(0, 0, 0, 35%);
+    border-left: 5px solid aliceblue;
+  }
+
+  .header-container.outro .button-letter{
+    color: aliceblue;
+  }
+
+  @media only screen and (max-width: 1200px) {
+
+    .header-container {
+      flex-direction: row;
+      height: unset;
+      width: 100%;
+      position: sticky;
+      top: 0;
+    }
+
+    .button-letter {
+      margin: 15px 10px;
+    }
+
+    .header-container.outro {
+      border-left: none;
+      border-top: 2px solid aliceblue;
+    }
+  }
+</style>
--- src/components/Schedule/Schedule.svelte
+++ src/components/Schedule/Schedule.svelte
@@ -3,7 +3,8 @@
 	import page from 'page'
   import anime from 'animejs/lib/anime.es.js'
 
-	import scheduleTransitions from '../../transitions/schedule'
+  import scheduleTransitions from '../../transitions/schedule'
+  import Header from '../Header/Header.svelte'
   import Day from './Day/Day.svelte'
   import { mobile } from '../../stores'
 
@@ -41,11 +42,7 @@
 
 
 <div class='schedule'>
-  <div class='header-container' class:outro>
-    {#each "SCHEDULE".split("") as letter }
-      <h3 class='button-letter'>{letter}</h3>
-    {/each}
-  </div>
+  <Header headerText='SCHEDULE' outro={outro} />
   <div class='schedule-content'
   bind:clientHeight={height}
   bind:clientWidth={width}>
@@ -71,25 +68,6 @@
     overflow: hidden;
   }
 
-  .header-container {
-    display: flex;
-    height: 100%;
-    z-index: 1;
-    flex-direction: column;
-    justify-content: center;
-    align-items: center;
-    padding: 6.4px;
-    background-color: aliceblue;
-    transition: background-color 1s ease-in-out 0.45s, border-right 1s ease-in-out 0.45s;
-  }
-
-  .button-letter {
-    margin: 10px 30px;
-    color: black;
-    text-shadow: none;
-    transition: color 1s ease-in-out 0.45s;
-  }
-
   .schedule-content {
     display: flex;
     flex: 1;
@@ -100,41 +78,15 @@
     overflow-x: hidden;
   }
 
-  .header-container.outro {
-    background-color: rgba(0, 0, 0, 35%);
-    border-left: 5px solid aliceblue;
-  }
-
-  .header-container.outro .button-letter{
-    color: aliceblue;
-  }
-
   @media only screen and (max-width: 1200px) {
     .schedule {
       flex-direction: column;
-    }
-
-    .header-container {
-      flex-direction: row;
-      height: unset;
-      width: 100%;
-      position: sticky;
-      top: 0;
     }
 
     .schedule-content {
       flex-direction: column;
       justify-content: flex-start;
       overflow-y: scroll;
-    }
-
-    .button-letter {
-      margin: 15px 10px;
-    }
-
-    .header-container.outro {
-      border-left: none;
-      border-top: 2px solid aliceblue;
     }
   }
 </style>
